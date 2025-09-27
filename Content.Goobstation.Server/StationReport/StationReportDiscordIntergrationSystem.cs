1ad90b9c41906d113b16f13ed5aad8872e0f84c2
Timfa <timfalken@hotmail.com>
2025-09-27 02:55:22 +0200

    fix [color=#FFaa44] tags in Discord Station Report (#154)
    Update StationReportDiscordIntergrationSystem.cs

--- Content.Goobstation.Server/StationReport/StationReportDiscordIntergrationSystem.cs
+++ Content.Goobstation.Server/StationReport/StationReportDiscordIntergrationSystem.cs
@@ -58,7 +58,7 @@ public sealed class StationReportDiscordIntergrationSystem : EntitySystem
         new(@"\[head=3\]", @""), // Omu, make head be replaced with empty, was ### 
         new(@"\[head=4\]", @""), // Omu, make head be replaced with empty, was -# 
         new(@"\[/head\]", @""),
-        new(@"\[/?color(=[#0-9a-zA-Z]+)?\]", @"")
+        new(@"\[/?color=?([#0-9a-zA-Z]+)?\]", @"") // Omu, fix colour tag regex
     };
 
     private void OnStationReportReceived(StationReportEvent ev)
